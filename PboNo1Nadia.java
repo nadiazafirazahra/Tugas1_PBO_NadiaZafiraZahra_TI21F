a45c61cb75290c7035db9e770ddf03a702fe8bb5
nadiazafirazahra <114532973+nadiazafirazahra@users.noreply.github.com>
2022-09-28 08:47:13 +0700

    Add files via upload

---
+++ PboNo1Nadia.java
@@ -0,0 +1,11 @@
+/**
+ *
+ * @author Acer
+ */
+public class PboNo1Nadia {
+    public static void main(String[] args) {
+        System.out.println("Belajar JAVA");
+        System.out.println("Sangat mudah sekali");
+        System.out.println("dan sangat menyenangkan");
+    } 
+}
---
+++ PboNo2.java
@@ -0,0 +1,12 @@
+
+import javax.swing.JOptionPane;
+
+public class PboNo2 {
+ public static void main(String[] args) {
+        String input;
+        input = JOptionPane.showInputDialog("Anda Sedang belajar apa?");
+        JOptionPane.showMessageDialog(null, "Belajar"+input+" sangat mudah","Message",JOptionPane.INFORMATION_MESSAGE);
+        
+        
+    }
+}
